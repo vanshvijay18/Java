98bd17f796b659f5a292f47657cb2b00de828c94
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-01-20 23:55:05 +0530

    Add ReadLineByLine class to read file line by line

---
+++ FileHandling/ReadLineByLine.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class ReadLineByLine {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+        String line;
+
+        while((line = br.readLine()) != null){
+            System.out.println(line);
+        }
+
+        br.close();
+    }
+}
