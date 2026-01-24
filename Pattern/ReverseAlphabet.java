8dca3961bc8ff45f14dc07ac5fcbdeadddf94563
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-01-24 21:56:57 +0530

    Add ReverseAlphabet class to print reverse alphabet pattern
    A B C D E 
    A B C D 
    A B C 
    A B 
    A

---
+++ Pattern/ReverseAlphabet.java
@@ -0,0 +1,13 @@
+public class ReverseAlphabet {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = n; i >= 1; i--){
+            char ch = 'A';
+            for(int j = 1; j <= i; j++){
+                System.out.print(ch++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
