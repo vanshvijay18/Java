8c9e2e111369302f388effad803a0e839f15ce91
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-01-25 23:40:30 +0530

    Add TrianglePattern1.java to print a triangle pattern
     19            
     17 15         
     13 11  9      
      7  5  3  1

---
+++ Pattern/TrianglePattern1.java
@@ -0,0 +1,18 @@
+public class TrianglePattern1 {
+    public static void main(String[] args) {
+        int n = 4;
+        int num = 19;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(j <= i){
+                    System.out.printf("%3d", num);
+                    num -= 2;
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
