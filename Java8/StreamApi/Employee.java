08fea99f9b37eacaa711262e92046b99b2403c0f
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-01-28 21:40:01 +0530

    Add files via upload

---
+++ Java8/StreamApi/Employee.java
@@ -0,0 +1,89 @@
+package StreammJava8;
+
+public class Employee {
+	private int id;
+	String name;
+	private int age;
+	private String gender;
+	private String department;
+	private int yearOfJoining;
+	private double salary;
+
+	// Constructor
+	public Employee(int id, String name, int age, String gender, String department, int yearOfJoining, double salary) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+
+	// Getters
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public int getYearOfJoining() {
+		return yearOfJoining;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public void setYearOfJoining(int yearOfJoining) {
+		this.yearOfJoining = yearOfJoining;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]" +" \n";
+	}
+
+	
+	
+	
+}
