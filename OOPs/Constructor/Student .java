d282c9cf21cdf7f5fc7b3743e88637c4f751ac2d
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-12 19:56:01 +0530

    Default Constructor
    A constructor with no parameters.

---
+++ OOPs/Constructor/Student .java
@@ -0,0 +1,9 @@
+class Student {
+    Student() {   // default constructor
+        System.out.println("Default constructor called");
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+    }
+}
