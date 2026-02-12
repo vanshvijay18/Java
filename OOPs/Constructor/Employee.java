2c0287618f40700e69e466e90a524f6df5b58ec8
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-12 19:57:23 +0530

    Constructor Overloading
    Multiple constructors in the same class with different parameters.

---
+++ OOPs/Constructor/Employee.java
@@ -0,0 +1,13 @@
+class Employee {
+    int id;
+    String name;
+
+    Employee() {
+        System.out.println("Default constructor");
+    }
+
+    Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
