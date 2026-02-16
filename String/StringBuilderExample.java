26052b57140b4dbcf14e067e8cc293bc3749f1be
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-16 22:50:23 +0530

    Add StringBuilderExample class to demonstrate usage
    StringBuilder is mutable and faster, but not thread-safe

---
+++ String/StringBuilderExample.java
@@ -0,0 +1,8 @@
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+        sb.append(" World");
+
+        System.out.println(sb); // Java World
+    }
+}
