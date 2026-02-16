23f71b9b1c434b09dc2ba28a5a48a6e78db21abb
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-16 22:49:33 +0530

    Add StringBufferExample class to demonstrate StringBuffer
    StringBuffer is mutable and thread-safe (synchronized).

---
+++ String/StringBufferExample.java
@@ -0,0 +1,8 @@
+public class StringBufferExample {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Java");
+        sb.append(" World");
+
+        System.out.println(sb); // Java World
+    }
+}
