c9d5f2dda6ccc595ed7859362b015474d763b3b1
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-16 22:48:53 +0530

    String is immutable, so original value does not change.
    The string concatenation does not modify the original string.

---
+++ String/StringExample.java
@@ -0,0 +1,8 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String s = "Java";
+        s.concat(" World");
+
+        System.out.println(s); // Java
+    }
+}
