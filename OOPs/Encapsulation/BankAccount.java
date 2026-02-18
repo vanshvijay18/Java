023735f20e87f0a6f1831583989700297136706e
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-18 14:38:16 +0530

    Implement BankAccount class with deposit and balance methods

---
+++ OOPs/Encapsulation/BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+    private double balance;
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
