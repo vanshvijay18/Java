9c40c5343e947772f3a63c79c17c4c13084941ca
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-20 22:04:43 +0530

    Add PalindromeString class to check palindromes

---
+++ String/PalindromeStrin.java
@@ -0,0 +1,12 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String s = "madam";
+        String rev = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+
+        System.out.println(s.equals(rev));
+    }
+}
