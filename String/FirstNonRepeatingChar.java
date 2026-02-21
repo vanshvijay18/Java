75ef2c99a5dbe0bfd78c24bf0881402361d36dde
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-21 20:31:57 +0530

    Add FirstNonRepeatingChar class to find unique char

---
+++ String/FirstNonRepeatingChar.java
@@ -0,0 +1,12 @@
+public class FirstNonRepeatingChar {
+    public static void main(String[] args) {
+        String s = "swiss";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))) {
+                System.out.println(s.charAt(i));
+                break;
+            }
+        }
+    }
+}
