4802f841bee798305d15aa0e8b7117d45c2af493
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-22 22:14:37 +0530

    Add AnagramCheck class to verify anagrams

---
+++ String/AnagramCheck.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+
+        char[] a1 = s1.toCharArray();
+        char[] a2 = s2.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+
+        System.out.println(Arrays.equals(a1, a2));
+    }
+}
