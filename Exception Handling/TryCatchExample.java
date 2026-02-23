c188ecf84d824b6720379692bfe3c02a1b55aa5d
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-23 21:57:21 +0530

    Add TryCatchExample for handling division by zero

---
+++ Exception Handling/TryCatchExample.java
@@ -0,0 +1,9 @@
+public class TryCatchExample {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
